d273576ad42695c6a39fff153e6bb46c7bf23442
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-11-03 13:03:06 +0800

    Update SocketUtil.java

--- SocketUtil.java
+++ SocketUtil.java
@@ -95,7 +95,7 @@ public class SocketUtil extends AndroidNonvisibleComponent {
    }
 
     @SimpleEvent
-    public void GetMessage(string s){
+    public void GetMessage(Int s){
         EventDispatcher.dispatchEvent(this, "GetMessage", "\n"+s);
     }
     @SimpleFunction(description = "start")
