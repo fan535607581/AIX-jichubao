a34a50c809cf48396945e983251b6602c69ee573
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-11-07 17:01:33 +0800

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -76,7 +76,7 @@ public class SocketClient extends AndroidNonvisibleComponent {
         }
     }
     @SimpleFunction(description = "start")
-    public void sendMessage(byte[] s){
+    public void sendMessage(byte s){
         if(socket != null){
             mt = new MyThread(SENDMESSAGE);
             mt.setText(s);
@@ -109,8 +109,8 @@ public class SocketClient extends AndroidNonvisibleComponent {
         public MyThread(int flag) {
             this.flag = flag;
         }
-        public void setText(byte[] s){
-            i = s;
+        public void setText(byte s){
+            i[0]= s;
         }
         public void setIP(String ip){
             IP = ip;
