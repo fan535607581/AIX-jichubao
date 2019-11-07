497fd4deff03a1f3cc66f240598ae518c3bf9c07
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-11-07 13:26:03 +0800

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -76,7 +76,7 @@ public class SocketClient extends AndroidNonvisibleComponent {
         }
     }
     @SimpleFunction(description = "start")
-    public void sendMessage(byte[] s){
+    public void sendMessage(byte s){
         if(socket != null){
             mt = new MyThread(SENDMESSAGE);
             mt.setText(s);
@@ -102,7 +102,7 @@ public class SocketClient extends AndroidNonvisibleComponent {
     }
     class MyThread extends Thread {
  
-        public byte[] txt1;
+        public byte txt1;
         public String IP;
         public byte[]i=new byte[]{11};
         Message msg;
@@ -110,7 +110,7 @@ public class SocketClient extends AndroidNonvisibleComponent {
         public MyThread(int flag) {
             this.flag = flag;
         }
-        public void setText(byte[] s){
+        public void setText(byte s){
             txt1 = s;
         }
         public void setIP(String ip){
@@ -144,7 +144,7 @@ public class SocketClient extends AndroidNonvisibleComponent {
                 break;
                 case SENDMESSAGE:
                     try {
-                        i[0]=txt1[0];
+                        i[0]=txt1;
                         ou.write(i[0]);//       
                         msg = myHandler.obtainMessage();
                         msg.obj = "发送完毕";
