a71ef1865b63d38323a0dd8f75417850e1f5a3c1
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-11-07 15:13:38 +0800

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -144,7 +144,7 @@ public class SocketClient extends AndroidNonvisibleComponent {
                 break;
                 case SENDMESSAGE:
                     try {
-                        byte[]i={0,1,2,3,4,5,6,7,8,9,10,11,12};
+                        i=txt1;
                         ou.write(i[0]);//
                         ou.write(i[1]);
                         ou.write(i[2]);
