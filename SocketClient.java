d8c901145b9a7752de351019dda3a082c853c294
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-11-07 18:37:31 +0800

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -79,7 +79,7 @@ public class SocketClient extends AndroidNonvisibleComponent {
     public void sendMessage(byte s , byte b){
         if(socket != null){
             mt = new MyThread(SENDMESSAGE);
-            mt.setText(s,b);mt.setText(b,s);
+            mt.setText(s,b);mt.setText(b,b);
             mt.start();
         }else{
             GetMessage("连接未创建！");
