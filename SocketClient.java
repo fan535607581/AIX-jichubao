c048251479ddab564f280697a5a1f27b989c5816
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-11-08 08:45:14 +0800

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -104,7 +104,7 @@ public class SocketClient extends AndroidNonvisibleComponent {
     class MyThread extends Thread {
  
         public String IP;
-        public byte[]i=new byte[]{13};
+        public byte[]i=new byte[13];
         Message msg;
         public int flag;
         public MyThread(int flag) {
