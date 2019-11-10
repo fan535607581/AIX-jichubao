153407a45bfa4b1477ab07a803c467c0a9ebcfd8
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-11-10 09:56:27 +0800

    Update SocketUtil.java

--- SocketUtil.java
+++ SocketUtil.java
@@ -148,7 +148,6 @@ public class SocketUtil extends AndroidNonvisibleComponent {
 			message_2 = handler.obtainMessage();
                         message_2.obj = msg;
                         handler.sendMessage(message_2);
-			br.close();
                     }
 			
                 }
