5d634a948c47519e36bdd90810584abfd80370ab
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-11-11 13:05:10 +0800

    Update SocketUtil.java

--- SocketUtil.java
+++ SocketUtil.java
@@ -140,7 +140,7 @@ public class SocketUtil extends AndroidNonvisibleComponent {
 	    public void run() {
             try {
                BufferedReader br = null;
-		br = new BufferedReader(new InputStreamReader());
+		br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 while(true)
 		{	
                     int msg = 0;
