3f3e03a25b822564f8c98b44114ad5c5f27c76bf
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-11-11 22:43:07 +0800

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -39,7 +39,7 @@ import java.net.SocketTimeoutException;
 
 
 
-@DesignerComponent(version = 1,
+@DesignerComponent(version = 2,
     description = " made in fan hao jie    \n   e-mail:535607581@qq.com ",
     category = ComponentCategory.EXTENSION,
     nonVisible = true,
