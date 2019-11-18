80680de5117ec93e1915270e45a61679fa600deb
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-11-18 16:09:32 +0800

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -66,16 +66,6 @@ public class SocketClient extends AndroidNonvisibleComponent {
         }
  
     };
-	//////////////////////////////////
-	public Handler handler = new Handler()
-    {
-        @Override
-        public void handleMessage(Message msg)
-	{
-        	GetMessage(msg.obj.toString());
-        }
-    };
-	/////////////////////////////////////
     @SimpleFunction(description = "start")
     public void closeConnect(){
         if(socket != null){
@@ -145,12 +135,12 @@ public class SocketClient extends AndroidNonvisibleComponent {
                 msg = br.read();   msk = msg;     msb = msg>>8;      msg = msg&0xff;
                 if(msk > -1)
                 {
-                    message_2 = handler.obtainMessage();
+                    message_2 = myhandler.obtainMessage();
                     message_2.obj = msb;
-                    handler.sendMessage(message_2);
-                    message_2 = handler.obtainMessage();
+                    myhandler.sendMessage(message_2);
+                    message_2 = myhandler.obtainMessage();
                     message_2.obj = msg;
-                    handler.sendMessage(message_2);
+                    myhandler.sendMessage(message_2);
                 }
                 else{  socket.close();  br.close();} 
                 } 
