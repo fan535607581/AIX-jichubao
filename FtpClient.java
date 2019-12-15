63c0d8c2b77ad554c8d86c38c75d46bd8bd0ac8b
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-12-15 10:45:58 +0800

    Update FtpClient.java

--- FtpClient.java
+++ FtpClient.java
@@ -1,33 +1,137 @@
-package FTP;
+package com.cloudpower.util;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-public void run() {
-    public static void main(String[] args) {
-        String ftpHost = "172.203.160.104";
-        String ftpUserName = "GBNFTP";
-        String ftpPassword = "ASDFGHJKL;'";
-        int ftpPort = 21;
-        String ftpPath = "/FTP/FTPDOWN";
-        String localPath = "D:\\FTP\\员工信息表.txt";//"D:\\FTP\\upload.txt";
-        String downloadPath = "D:\\FTP";
-        String uploadFileName = "员工上传.txt";//"ftp_upload.txt";
-        String downloadFileName = "员工下载.txt";//"ftp_download.txt";
+import sun.net.TelnetInputStream;
+import sun.net.TelnetOutputStream;
+import sun.net.ftp.FtpClient;
 
-        // 上传一个文件
-        try {
-            System.out.println("=============上传文件操作===============");
-            FileInputStream in = new FileInputStream(new File(localPath));
-            boolean isUp = FtpUtil.uploadFile(ftpHost, ftpUserName, ftpPassword, ftpPort, ftpPath, uploadFileName, in);
-            if (isUp) {
-                System.out.println("上传成功 !");
-            } else {
-                System.out.println("上传失败 !");
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println(e);
-        }
+public class Ftp {
 
-    }
+	private String localfilename; //本地文件名	
+	private String remotefilename; //远程文件名
+	private FtpClient ftpClient; //FTP客户端
+
+	/* @param user 用户名* @param password  密码 * @param path  服务器路径 * @author 周玲斌 */
+	public void connectServer(String ip, int port, String user,
+			String password, String path) {
+		try {
+			/* ******连接服务器的两种方法****** */
+			// 第一种方法
+			// ftpClient = new FtpClient();
+			// ftpClient.openServer(ip, port);
+			// 第二种方法
+			ftpClient = new FtpClient(ip);
+
+			ftpClient.login(user, password);
+			// 设置成2进制传输
+			ftpClient.binary();
+			System.out.println("login success!");
+			if (path.length() != 0) {
+				// 把远程系统上的目录切换到参数path所指定的目录
+				ftpClient.cd(path);
+			}
+			ftpClient.binary();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			throw new RuntimeException(ex);
+		}
+	}
+
+	/**
+	 * 关闭连接
+	 * 
+	 * @author 周玲斌
+	 * @date 2012-7-11
+	 */
+	public void closeConnect() {
+		try {
+			ftpClient.closeServer();
+			System.out.println("disconnect success");
+		} catch (IOException ex) {
+			System.out.println("not disconnect");
+			ex.printStackTrace();
+			throw new RuntimeException(ex);
+		}
+	}
+
+	/**
+	 * 上传文件
+	 * 
+	 * @param localFile
+	 *            本地文件
+	 * @param remoteFile
+	 *            远程文件
+	 * @author 周玲斌
+	 * @date 2012-7-11
+	 */
+	public void upload(String localFile, String remoteFile) {
+		this.localfilename = localFile;
+		this.remotefilename = remoteFile;
+		TelnetOutputStream os = null;
+		FileInputStream is = null;
+		try {
+			// 将远程文件加入输出流中
+			os = ftpClient.put(this.remotefilename);
+			// 获取本地文件的输入流
+			File file_in = new File(this.localfilename);
+			is = new FileInputStream(file_in);
+			// 创建一个缓冲区
+			byte[] bytes = new byte[1024];
+			int c;
+			while ((c = is.read(bytes)) != -1) {
+				os.write(bytes, 0, c);
+			}
+			System.out.println("upload success");
+		} catch (IOException ex) {
+			System.out.println("not upload");
+			ex.printStackTrace();
+			throw new RuntimeException(ex);
+		} finally {
+			try {
+				if (is != null) {
+					is.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (os != null) {
+						os.close();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+
+	}
+
+	public static void main(String agrs[]) {
+
+		String filepath[] = { "/temp/aa.txt", "/temp/regist.log" };
+		String localfilepath[] = { "C:\\tmp\\1.txt", "C:\\tmp\\2.log" };
+
+		Ftp fu = new Ftp();
+		/*
+		 * 使用默认的端口号、用户名、密码以及根目录连接FTP服务器
+		 */
+		fu.connectServer("127.0.0.1", 22, "anonymous", "IEUser@", "/temp");
+
+		// 下载
+		for (int i = 0; i < filepath.length; i++) {
+			fu.download(filepath[i], localfilepath[i]);
+		}
+
+		String localfile = "E:\\号码.txt";
+		String remotefile = "/temp/哈哈.txt";
+		// 上传
+		fu.upload(localfile, remotefile);
+		fu.closeConnect();
+	}
 }
