edca512a94f254e278bdb064c8fb03cadc3d1238
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-12-15 09:23:52 +0800

    Update and rename SocketClient.java to FtpClient.java

---
+++ FtpClient.java
@@ -0,0 +1,44 @@
+package FTP;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String ftpHost = "172.203.160.104";
+        String ftpUserName = "GBNFTP";
+        String ftpPassword = "ASDFGHJKL;'";
+        int ftpPort = 21;
+        String ftpPath = "/FTP/FTPDOWN";
+        String localPath = "D:\\FTP\\员工信息表.txt";//"D:\\FTP\\upload.txt";
+        String downloadPath = "D:\\FTP";
+        String uploadFileName = "员工上传.txt";//"ftp_upload.txt";
+        String downloadFileName = "员工下载.txt";//"ftp_download.txt";
+
+        // 上传一个文件
+        try {
+            System.out.println("=============上传文件操作===============");
+            FileInputStream in = new FileInputStream(new File(localPath));
+            boolean isUp = FtpUtil.uploadFile(ftpHost, ftpUserName, ftpPassword, ftpPort, ftpPath, uploadFileName, in);
+            if (isUp) {
+                System.out.println("上传成功 !");
+            } else {
+                System.out.println("上传失败 !");
+            }
+            System.out.println("=============下载文件操作===============");
+            // 下载一个文件
+            boolean isDown = FtpUtil.downloadFtpFile(ftpHost, ftpUserName, ftpPassword, ftpPort, ftpPath, downloadPath,
+                    uploadFileName, downloadFileName);
+            if (isDown) {
+                System.out.println("下载成功 !");
+            } else {
+                System.out.println("下载失败 !");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+
+    }
+}
+
+Main.java
