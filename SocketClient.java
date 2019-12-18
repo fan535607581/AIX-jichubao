c5346b8e0edb3b081292a31fbd6651048653b310
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-12-18 12:48:49 +0800

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -1,7 +1,5 @@
 package ftp.ke.hu;
 
-package cn.temp;
-
 import com.google.appinventor.components.annotations.*;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.runtime.*;
@@ -15,11 +13,10 @@ import com.google.appinventor.components.runtime.errors.YailRuntimeError;
     iconName = "images/extension.png")
 
 @SimpleObject(external = true)
-
-public class MyExtension extends AndroidNonvisibleComponent {
+public class MyExtension extends AndroidNonvisibleComponent 
+{
     public static final int VERSION = 1;
     private static final String LOG_TAG = "MyExtension";
 
-    public MyExtension(ComponentContainer container) {
-        super(container.$form());
-    }
+    public MyExtension(ComponentContainer container) {super(container.$form());}
+}
