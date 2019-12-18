0e5392137af462f83f4e2ddecbf8cb07da457051
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2019-12-18 12:41:57 +0800

    Update and rename ftpkuhuduan.java to SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -49,7 +49,7 @@ import java.net.*;
 
 @SimpleObject(external = true)
 
-public class ftpkuhuduan extends AndroidNonvisibleComponent 
+public class SocketClient extends AndroidNonvisibleComponent 
 {
     Socket socket = null;
     OutputStream ou = null;
